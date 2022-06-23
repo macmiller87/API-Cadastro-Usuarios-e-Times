9dace72690357ded488a3f2564082ec879f8d3c2
macmiller87 <macamagolf@gmail.com>
2022-06-23 00:17:34 -0300

    Feat: criando usecase listuser

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,11 @@
 import { Router } from "express";
 import { listUserRoute, UsersRoute } from "./Users.routes";
 import { AuthenticateUserRoute } from "./AuthenticateUser.routes";
-import { createUserTeamsRoute } from "./CreateUserTeams.routes";
 
 const router = Router();
 
 router.use("/createUser", UsersRoute);
 router.use("/profile", listUserRoute);
 router.use("/sessions", AuthenticateUserRoute);
-
-router.use("/createUserTeams/:user_id", createUserTeamsRoute);
 
 export { router };
