23fbda8886c6c1c1bdb612a01e74a449970c8452
macmiller87 <macamagolf@gmail.com>
2022-06-24 03:41:57 -0300

    Refactor: usecase Createusers e listuser

--- src/routes/Users.routes.ts
+++ src/routes/Users.routes.ts
@@ -2,21 +2,16 @@ import { Router } from "express";
 import { CreateUsersController } from "@modules/CreateUsers/useCase/CreateUser/CreateUsersController";
 import { ListUserController } from "@modules/CreateUsers/useCase/ListUser/ListUserController";
 import { ensureUserAuthenticate } from "@utils/auth/EnsureUserAuthenticate";
-import { ListUserTeamsController } from "@modules/CreateUsers/useCase/ListUserTeams/ListUserTeamsController";
 
 const CreateUserRoute = Router();
 const listUserRoute = Router(); 
-const listUserAndTeamsRoute = Router();
 
 const createUsers = new CreateUsersController();
 const listUserController = new ListUserController();
-const listUserteamsController = new ListUserTeamsController();
 
 listUserRoute.use(ensureUserAuthenticate);
-listUserAndTeamsRoute.use(ensureUserAuthenticate);
 
 CreateUserRoute.post("/", createUsers.handle);
 listUserRoute.get("/", listUserController.handle);
-listUserAndTeamsRoute.get("/", listUserteamsController.handle);
 
-export { CreateUserRoute, listUserRoute, listUserAndTeamsRoute };
+export { CreateUserRoute, listUserRoute };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { AuthenticateUserRoute } from "./AuthenticateUser.routes";
-import { CreateUserRoute, listUserRoute, listUserAndTeamsRoute, } from "./Users.routes";
+import { CreateUserRoute, listUserRoute } from "./Users.routes";
 import { createUserTeamsRoute, listSpecifTeamRoute } from "./CreateUserTeams.routes";
 
 const router = Router();
@@ -8,7 +8,6 @@ const router = Router();
 router.use("/createUser", CreateUserRoute);
 router.use("/sessions", AuthenticateUserRoute);
 router.use("/profile", listUserRoute);
-router.use("/profileUserAndTeams", listUserAndTeamsRoute);
 
 router.use("/createUserTeams", createUserTeamsRoute);
 router.use("/listSpecifTeam", listSpecifTeamRoute);
