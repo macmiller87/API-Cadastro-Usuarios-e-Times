a9650a20dbd844f660a9f19a7a79f1cb7965bfd4
macmiller87 <macamagolf@gmail.com>
2022-06-27 23:59:36 -0300

    Build: Criando configuracao para testes com jest

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -10,16 +10,14 @@ import { CreateUser1655711870659 } from "./migrations/1655711870659-CreateUser";
 import { CreateUsersToken1655784996610 } from "./migrations/1655784996610-CreateUsersToken";
 import { CreateUserTeams1655754301909 } from "./migrations/1655754301909-CreateUserTeams";
 
-
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.POSTGRES_HOST,
     port: 5432,
     synchronize: false,
     logging: false,
     username: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.NODE_ENV === "test" ? "dbtest" : process.env.POSTGRES_DATABASE,
     entities: [Users, Teams, UsersToken],
     subscribers: [],
     migrations: [
@@ -29,11 +27,11 @@ export const AppDataSource = new DataSource({
     ]
 });
 
-export async function createConnection() {
+export async function createConnection(host = process.env.POSTGRES_HOST): Promise<DataSource> {
     
     try{
-        await AppDataSource.initialize();
         console.log("Database Connected 📦");
+        return await AppDataSource.setOptions({ host }).initialize();
     }catch(error){
         console.log("Error During Connection", error);
     }
